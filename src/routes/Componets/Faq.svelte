a8fc352a91ce42b213c5028ffeaef12838cfa098
JSaretin <jsaretin@gmail.com>
2023-11-15 08:33:29 +0100

    main layout

---
+++ src/routes/Componets/Faq.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import Minus from "./Minus.svelte";
+  import Plus from "./Plus.svelte";
+
+  export let faq: string;
+  export let expand: boolean = false;
+</script>
+
+<div class="overflow-hidden rounded-md shadow-sm shadow-yellow-500">
+  <button
+    class={"p-2 w-full flex justify-between " +
+      (expand ? "bg-yellow-500 text-gray-950" : "text-yellow-600")}
+    on:click
+  >
+    <h3 class="lg:text-lg align-middle place-items-center">
+      {faq}
+    </h3>
+    {#if expand}
+      <Minus />
+    {:else}
+      <Plus />
+    {/if}
+  </button>
+
+  {#if expand}
+    <p class="p-2 leading-relaxed text-yellow-700">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+      facilis cumque vitae repudiandae facere, porro eaque, animi, atque beatae
+      alias numquam quos? Quisquam veritatis delectus excepturi fugiat ullam at
+      modi.
+    </p>
+  {/if}
+</div>
